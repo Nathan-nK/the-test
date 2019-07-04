5398f8495b331f2796dfd52661677ee5c0bc1551
Nathan Kleshitsky <nathankls97@gmail.com>
2019-07-04 11:27:13 +0300

    Finished first API request

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require( 'express' )
 const app = express()
-const port = 100
+const port = 8080
 const request = require('request')
 const path = require( 'path' )
 
-app.use( express.static( path.join( __dirname, 'public' ) ) )
+
+app.use( express.static( path.join( __dirname, 'dist' ) ) )
+app.use( express.static( path.join( __dirname, 'node_modules' ) ) )
 app.listen( port, () => console.log( `Example app listening on port ${ port }!` ) )
 
-
+app.get('/recipes/:ingredient', function (req, res) {
+    let ingredient = req.params.ingredient
+    request.get(`https://recipes-goodness.herokuapp.com/recipes/${ingredient}`, function (error, response){
+            console.log(response)
+    })
+})
 
 
 
